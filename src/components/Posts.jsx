f5a427e973b2eeea545ec9573e316fa01eaea633
Andrew Kwak <andrewebdevelop@gmail.com>
2018-12-03 17:03:16 -0500

    Changes to Posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import { List, Avatar, Icon } from 'antd';
+import { List, Icon } from 'antd';
 
 const Posts = ( { posts } ) => {
-  const postsData = [];
-  for (let i = 0; i < posts.length; i++) {
-    postsData.push( {
-      href: `https://reddit.com${ posts[i].permalink }`,
-      title: posts[i].data.title,
-      avatar: posts[i].data.thumbnail,
-      description: posts[i].data.subreddit,
-      content: posts[i].data.selftext,
-      upvotes: posts[i].data.ups,
-    } );
-  }
-  console.log( postsData );
   const IconText = ({ type, text }) => (
     <span>
       <Icon type={type} style={{ marginRight: 8 }} />
@@ -23,25 +11,25 @@ const Posts = ( { posts } ) => {
   return (
     <List
       itemLayout="vertical"
+      size="medium"
       pagination={{
         onChange: ( page ) => {
           console.log( page );
         },
         pageSize: 10,
       }}
-      dataSource={ postsData }
-      renderItem={ item => (
+      dataSource={ posts }
+      renderItem={ post => (
         <List.Item
-          key={item.title}
-          actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text={ item.ups } />, <IconText type="message" text="2" />]}
-          extra={<img width={200} alt="logo" src={ item.avatar } />}
+          key={post.data.title}
+          actions={[<IconText type="like-o" text={ post.data.ups } />, <IconText type="message" text={ post.data.num_comments } />]}
+          extra={<img width={post.data.thumbnail_width} height={ post.data.thumbnail_height } alt="logo" src={ post.data.thumbnail } />}
         >
           <List.Item.Meta
-            avatar={<Avatar src={item.avatar} />}
-            title={<a href={item.href}>{item.title}</a>}
-            description={item.description}
+            title={<a href={`https://reddit.com${ post.data.permalink }`}>{post.data.title}</a>}
+            description={post.data.author}
           />
-          {item.content}
+          {post.data.selftext}
         </List.Item>
       )}
     />
--- src/containers/Reddit.jsx
+++ src/containers/Reddit.jsx
@@ -1,15 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import { Row, Col } from 'antd';
 import Posts from '../components/Posts';
 
 class Reddit extends Component {
   state = {
-    subreddit: [],
+    subreddit: 'all',
     posts: [],
   }
 
   componentDidMount() {
-    axios.get( 'https://www.reddit.com/r/all.json' )
+    axios.get( `https://www.reddit.com/r/${ this.state.subreddit }.json` )
       .then ( response => {
         if ( !this.state.posts.length ) {
           this.setState({
@@ -21,10 +22,14 @@ class Reddit extends Component {
   }
   render() {
     return (
-      <Fragment>
-        <p>Reddit</p>
-        <Posts posts={ this.state.posts } />
-      </Fragment>
+      <div>
+        <Row type="flex" justify="center">
+          <Col span={ 24 }><h1>Reddit</h1></Col>
+          <Col xs={ 24 } lg={ 10 }>
+            <Posts posts={ this.state.posts } />
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
